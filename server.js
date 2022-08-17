d1fba5b18a14b75b440a868dcdd3885675c4d5f4
haruto17 <s1902272@sendai-nct.jp>
2022-08-18 00:15:59 +0900

    ボタンを押すと緯度経度が取得できるようにしたよ

---
+++ server.js
@@ -0,0 +1,15 @@
+import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+
+serve((req) => {
+
+    const pathname = new URL(req.url).pathname;
+    console.log(pathname);
+
+    return serveDir(req, {
+        fsRoot: "public",
+        urlRoot: "",
+        showDirListing: true,
+        enableCors: true,
+    });
+});
